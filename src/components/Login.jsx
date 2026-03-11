941c17eb44a7465d0e1c6844398f2460b5a12053
cristiangrc <cgarciagarizabalo@gmail.com>
2026-03-11 11:19:45 -0400

    componente login

---
+++ src/components/Login.jsx
@@ -0,0 +1,65 @@
+import { useState,useEffect } from "react";
+
+
+function Login () {
+
+const [email,setEmail] = useState("")
+const [password,setPassword] = useState("")
+
+const handleLogin = async (e) => {
+    e.preventDefault();
+
+try {
+    
+    const res = await fetch ("/api/auth/login", {
+        method: "post",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    })
+    const data = await res.json(); // Convertimos la respuesta a objeto
+
+if (res.ok) {
+    // Si el login es correcto, guardamos el token
+    localStorage.setItem("token", data.token);
+    alert("Login exitoso");
+    window.location.href = "/tareas";
+} else {
+    // Si las credenciales son incorrectas
+    alert(data.error || "Error al iniciar sesión");
+}
+
+} catch (error) {
+    console.error("error al conectar",error)
+}
+
+}
+
+
+return (
+
+    <form className="login-container" onSubmit={handleLogin}>
+    <h2 className="login-title">Iniciar Sesión</h2>
+        <input
+            className="login-input"
+            type="email" 
+            placeholder="Correo electrónico" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)}
+            />
+
+        <input 
+            className="login-input"
+            type="password" 
+            placeholder="Contraseña" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+        />
+        <button type="submit" className="login-button">Entrar</button>
+    </form>
+)
+
+}
+
+
+export default Login;
+
